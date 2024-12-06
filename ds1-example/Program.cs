d167fc7b2cb4e261720313c2c4e337d487fd82b9
NicHanks <104238034+NicHanks@users.noreply.github.com>
2024-12-05 18:34:51 -0700

    uploading half of project

---
+++ ds1-example/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+///
+///Help! 
+///customers keep sending their orders in for big macs, but I don't have enough 
+///workers to fullfill all the orders at the same time! 
+///What do we do???
+///Could you put them all in a queue? That would be great! 
+///Thanks bunches!
+///
+
+class static string[] main {
+    private list[] _orders = {order1, order2, order3};
+    private list[] _ordersQueued;
+
+    public Queue(){
+        // here we will use a for loop to put each item in the list into the que! 
+        foreach (string i in _orders){
+            _ordersQueued.add(i); 
+        } 
+    } // don't for get to use the .add() method! 
+}
+
+///
+///Great work! Now since u are sooo good at software work, I have come to you for 
+///help on my virtual pez dispensor game! Right now it keeps on crashing...
+///but I'm thinking now with you here we can implement a queue to dispense 
+///only one pez at a time!! 
+///
+///if I put in a red, blue, and green pez, could you queue them? 
+///Thanks so much! 
+///
+
+///code
+
+
+
---
+++ ds1-solution/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public void QueueSolution{
+
+    private Queue _queuedPezes;
+    private list[] = {red, blue, green};
+    public QueuePezes(_listOfPezes) {
+        // TODO (turn list of pezes into a queue of pezes!)
+        
+    }
+
+    Console.WriteLine(ToString(_queuedPezes));
+}
